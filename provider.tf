c8b02320e857df8ea6dfea76a750242539e78299
Hardik Raut <155015767+HardikRaut478@users.noreply.github.com>
2025-09-19 14:32:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "ap-south-1"
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
